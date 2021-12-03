04835e72221550bb2b6995ef0895b7f3657d2aec
Martin Strohalm <martin.strohalm@thermofisher.com>
2021-12-03 18:21:07 +0100

    Add check and tag example

---
+++ examples/ex03-09_eds_tag_and_check.py
@@ -0,0 +1,35 @@
+#  Created by Martin Strohalm, Thermo Fisher Scientific
+
+# import module
+import pyeds
+
+# open result file using the 'with' statement
+with pyeds.EDS("data.cdResult") as eds:
+    
+    # read most abundant item
+    item = next(eds.Read("ConsolidatedUnknownCompoundItem",
+        order = "Area (Max.)",
+        desc = True,
+        limit = 1))
+    
+    # show original state
+    print("Checked:", item.Checked)
+    print("Tags:", item.Tags.Values)
+    
+    # check item
+    item.Check(True)
+    
+    # turn on some tags
+    item.Tag(0, True)
+    item.Tag(4, False)
+    
+    # show new state
+    print()
+    print("Checked:", item.Checked)
+    print("Tags:", item.Tags.Values)
+    
+    # backup data first!
+    eds.Report.Backup()
+    
+    # update item in result database
+    eds.Update([item], ["Checked", "Tags"])
