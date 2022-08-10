02410262d9264c12ef607e0160025b2e308500e4
Martin Strohalm <martin.strohalm@thermofisher.com>
2022-08-10 12:21:33 +0200

    Add EDS.ReadMany method to read items dat by specified IDs

---
+++ examples/ex03-10_eds_read_many.py
@@ -0,0 +1,21 @@
+#  Created by Martin Strohalm, Thermo Fisher Scientific
+
+# import module
+import pyeds
+
+# open result file using the 'with' statement
+with pyeds.EDS("data.cdResult") as eds:
+    
+    # read spectrum metadata only
+    items = eds.Read("MassSpectrumItem", exclude=["Spectrum"], limit=10)
+    
+    # filter spectra
+    spectrum_ids = []
+    for item in items:
+        if item.MSOrder == 2:
+            spectrum_ids.append(item.IDs)
+    
+    # load full spectrum data
+    items = eds.ReadMany("MassSpectrumItem", spectrum_ids)
+    for item in items:
+        print(item.Spectrum)
