7097546e97ce068ab5e13d5939742e9321b16f0c
Martin Strohalm <martin.strohalm@thermofisher.com>
2024-03-14 15:34:47 +0100

    Update examples for CD 3.3.3

--- examples/ex03-11_eds_read_details.py
+++ examples/ex03-11_eds_read_details.py
@@ -4,25 +4,25 @@
 import pyeds
 
 # For latest PD version, some data like spectra and traces are stored in external
-# file. This fine can be accessed the same way as a regular result.
+# file. This file can be accessed the same way as a regular result.
 
 # define result file
-report_file = "__USE_YOU_OWN_PD_FILE__.pdResult"
+result_file = "__USE_YOU_OWN_PD_FILE__.pdResult"
 
 # define details file
 details_file = "__USE_YOU_OWN_PD_FILE__.pdResultDetails"
 
 # open files using the 'with' statement
-with (pyeds.EDS(report_file) as report,
-      pyeds.EDS(details_file) as details):
+with (pyeds.EDS(result_file) as result_eds,
+      pyeds.EDS(details_file) as details_eds):
     
     # read PSMs and connected spectrum i nfo
-    for psm in report.ReadHierarchy(["PSMs", "MSnSpectrumInfo"], limits=5):
+    for psm in result_eds.ReadHierarchy(["PSMs", "MSnSpectrumInfo"], limits=5):
         print(psm.MassOverCharge, psm.Sequence, psm.XCorr)
         
         # get spectra IDs
         ids = [s.IDs for s in psm.Children]
         
         # read spectra from details
-        for spectrum in details.ReadMany("MassSpectrumItem", ids):
+        for spectrum in details_eds.ReadMany("MassSpectrumItem", ids):
             print(spectrum.Spectrum)
