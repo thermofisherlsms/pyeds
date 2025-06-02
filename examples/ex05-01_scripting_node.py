6fd1a2002e00104c832a812f48bbadd85c2ad4ec
Martin Strohalm <martin.strohalm@thermofisher.com>
2025-06-02 19:52:11 +0200

    Update scripting node example

--- examples/ex05-01_scripting_node.py
+++ examples/ex05-01_scripting_node.py
@@ -2,156 +2,204 @@
 
 # import modules
 import sys
-import os.path
 import re
 import pyeds
 import pyeds.scripting
 
+"""
+This example shows some of the scripting node basics utilizing pyeds.scripting
+tools.
 
-# node settings
-# Path to Executable: C:\Program Files\PythonXXX\python.exe
-# Command Line Arguments: ABSOLUTE_PATH_TO_THIS_FILE %NODEARGS%
+It reads compounds and predicted compositions and creates a new top-level table
+with unique formulas linked to corresponding compound(s). Additional columns is
+also created in the Compounds table to directly show number of predicted
+compositions. Using connection properties, once a unique formula is selected,
+the linked Compounds sub-table directly shows the rank and SFit value for each
+compound.
 
-# define simple formula pattern
-formula_pattern = re.compile("([A-Z][a-z]*)(\d*)?")
+Although this example utilizes the tools even for exporting the data, it is not
+required. Data can be formated and exported using any favourite data framework
+instead. In such case, tab-separated values should be used for export. The full
+expected export path of each table is accessible as table.DataFile. The main
+response definition should be exported using response.ExportResponse().
 
-# read node args
-nodeargs = pyeds.scripting.NodeArgs(sys.argv)
-
-# init formula container as {formula: ({compounds IDs}, {compositions IDs}, mw, {atom: count}, {compound ID: (rank, sfit)})}
-formulas = {}
+To run this example just add the post-processing Scripting Node into your
+workflow and provide some essential info. The "Path to Executable" should be set
+directly to your Python executable (e.g. C:\Program Files\Python311\python.exe).
+The "Command Line Arguments" should provide the absolute path to this script
+followed by the %NODEARGS% (e.g. D:\Tmp\ex05-01_scripting_node.py %NODEARGS%).
+"""
 
 # init table names
 TB_FORMULAS = "Unique Formulas"
 TB_COMPOUNDS = "Compounds"
 TB_COMPOSITIONS = "Predicted Compositions"
+TB_FORMULAS_COMPOUNDS = "Formulas_Compounds"
+TB_FORMULAS_COMPOSITIONS = "Formulas_Compositions"
 
-# show message
-print("Loading compounds and formulas...")
 
-# read data from result file using PyEDS
-with pyeds.EDS(nodeargs.ResultFilePath) as eds:
+def collect_formulas(result_path):
+    """Collects unique predicted compositions."""
     
-    # load all predicted compositions for all compounds
-    compounds = eds.ReadHierarchy(
-        path = [TB_COMPOUNDS, TB_COMPOSITIONS],
-        properties = {TB_COMPOUNDS: []})
+    # show message
+    print("Loading compounds and formulas...")
     
-    # collect unique formulas
-    for compound in compounds:
-        for composition in compound.Children:
+    # init formula container as {formula: ({compounds IDs}, {compositions IDs}, mw, {atom: count}, {compound ID: (rank, sfit)})}
+    formulas = {}
+    
+    # init counts container as [(compound ID, count),]
+    counts = []
+    
+    # define simple formula pattern
+    formula_pattern = re.compile("([A-Z][a-z]*)(\d*)?")
+    
+    # read data from result file using PyEDS
+    with pyeds.EDS(result_path) as eds:
+        
+        # load all predicted compositions for all compounds
+        compounds = eds.ReadHierarchy(
+            path = [TB_COMPOUNDS, TB_COMPOSITIONS],
+            properties = {TB_COMPOUNDS: []})
+        
+        # collect unique formulas
+        for compound in compounds:
             
-            # get previously stored formula
-            formula = formulas.get(composition.Formula, None)
+            # add count
+            counts.append((compound.ID, len(compound.Children)))
             
-            # process new formula
-            if not formula:
+            # process formulas
+            for composition in compound.Children:
                 
-                # insert new formula
-                formula = (set(), set(), composition.MolecularWeight, {}, {})
-                formulas[composition.Formula] = formula
+                # get previously stored formula
+                formula = formulas.get(composition.Formula, None)
                 
-                # parse formula
-                matches = formula_pattern.findall(composition.Formula)
-                for element, count in matches:
-                    count = int(count) if count else 1
-                    formula[3][element] = count + formula[3].get(element, 0)
-            
-            # add IDs (note that for each table ALL IDs must be used)
-            formula[0].add(compound.ID)  # compounds table has only single ID column
-            formula[1].add((composition.ID, composition.WorkflowID))  # compositions table has two ID columns
-            
-            # add scores
-            formula[4][compound.ID] = (composition.Rank, composition.SpectralFit)
+                # process new formula
+                if not formula:
+                    
+                    # insert new formula
+                    formula = (set(), set(), composition.MolecularWeight, {}, {})
+                    formulas[composition.Formula] = formula
+                    
+                    # parse formula
+                    matches = formula_pattern.findall(composition.Formula)
+                    for element, count in matches:
+                        count = int(count) if count else 1
+                        formula[3][element] = count + formula[3].get(element, 0)
+                
+                # add IDs (note that for each table ALL IDs must be used)
+                formula[0].add(compound.ID)  # compounds table has only single ID column
+                formula[1].add((composition.ID, composition.WorkflowID))  # compositions table has two ID columns
+                
+                # add scores
+                formula[4][compound.ID] = (composition.Rank, composition.SpectralFit)
+        
+        return formulas, counts
 
-# show message
-print("Filling tables...")
 
-# init data tables
-formulas_table = []
-formulas_compounds_table = []
-formulas_compositions_table = []
-
-# fill results tables
-formula_id = 0
-for formula in formulas:
+def init_response(response_path):
+    """Initializes all response tables."""
     
-    # increase formula ID
-    formula_id += 1
+    # show message
+    print("Initializing response...")
     
-    # get values
-    compound_ids, composition_ids, mw, atoms, scores = formulas[formula]
-    C = atoms.get('C', 0)
-    H = atoms.get('H', 0)
-    N = atoms.get('N', 0)
-    O = atoms.get('O', 0)
-    S = atoms.get('S', 0)
-    P = atoms.get('P', 0)
-    F = atoms.get('F', 0)
+    # init node response
+    response = pyeds.scripting.Response(response_path)
     
-    # add to main table
-    formulas_table.append(f"{formula_id}\t{formula}\t{mw}\t{C}\t{H}\t{N}\t{O}\t{S}\t{P}\t{F}")
+    # define formulas table
+    formulas_t = response.AddTable(TB_FORMULAS)
+    formulas_t.AddColumn("ID", pyeds.scripting.INT, pyeds.scripting.ID)
+    formulas_t.AddColumn("Formula", pyeds.scripting.STRING)
+    formulas_t.AddColumn("Molecular Weight", pyeds.scripting.FLOAT, FormatString="F5")
+    formulas_t.AddColumn("# Compounds", pyeds.scripting.INT)
+    formulas_t.AddColumn("# C", pyeds.scripting.INT)
+    formulas_t.AddColumn("# H", pyeds.scripting.INT)
+    formulas_t.AddColumn("# N", pyeds.scripting.INT)
+    formulas_t.AddColumn("# O", pyeds.scripting.INT)
+    formulas_t.AddColumn("# S", pyeds.scripting.INT)
+    formulas_t.AddColumn("# P", pyeds.scripting.INT)
+    formulas_t.AddColumn("# F", pyeds.scripting.INT)
     
-    # add to connection tables
-    for compound_id in compound_ids:
-        formulas_compounds_table.append(f"{formula_id}\t{compound_id}\t{scores[compound_id][0]}\t{scores[compound_id][1]:.0f}")
+    # define compounds update table
+    compounds_t = response.AddTable(TB_COMPOUNDS)
+    compounds_t.AddColumn("ID", pyeds.scripting.INT, pyeds.scripting.ID)
+    compounds_t.AddColumn("# Compositions", pyeds.scripting.INT, PositionAfter="Formula")
     
-    for composition_id in composition_ids:
-        formulas_compositions_table.append(f"{formula_id}\t{composition_id[0]}\t{composition_id[1]}")
+    # define formula to compounds link table
+    # make sure all ID columns starts with the table name followed by the ID column name
+    # note that for now, only string can be used for connection columns
+    formulas_compounds_t = response.AddConnection(TB_FORMULAS_COMPOUNDS, TB_FORMULAS, TB_COMPOUNDS)
+    formulas_compounds_t.AddColumn(f"{TB_FORMULAS} ID", pyeds.scripting.INT, pyeds.scripting.ID)
+    formulas_compounds_t.AddColumn(f"{TB_COMPOUNDS} ID", pyeds.scripting.INT, pyeds.scripting.ID)
+    formulas_compounds_t.AddColumn("Formula Rank", pyeds.scripting.STRING)
+    formulas_compounds_t.AddColumn("Formula SFit", pyeds.scripting.STRING, FormatString="F0")
+    
+    # define formula to compositions link table
+    # make sure all ID columns starts with the table name followed by the ID column name
+    formulas_compositions_t = response.AddConnection(TB_FORMULAS_COMPOSITIONS, TB_FORMULAS, TB_COMPOSITIONS)
+    formulas_compositions_t.AddColumn(f"{TB_FORMULAS} ID", pyeds.scripting.INT, pyeds.scripting.ID)
+    formulas_compositions_t.AddColumn(f"{TB_COMPOSITIONS} ID", pyeds.scripting.INT, pyeds.scripting.ID)
+    formulas_compositions_t.AddColumn(f"{TB_COMPOSITIONS} WorkflowID", pyeds.scripting.INT, pyeds.scripting.WORKFLOW_ID)
+    
+    return response
 
-# show message
-print("Creating response...")
 
-# init node response
-response = pyeds.scripting.Response(nodeargs.ExpectedResponsePath)
+def fill_tables(response, formulas, counts):
+    """Fills response tables."""
+    
+    # show message
+    print("Filling tables...")
+    
+    # get tables
+    compounds_t = response.GetTable(TB_COMPOUNDS)
+    formulas_t = response.GetTable(TB_FORMULAS)
+    formulas_compounds_t = response.GetTable(TB_FORMULAS_COMPOUNDS)
+    formulas_compositions_t = response.GetTable(TB_FORMULAS_COMPOSITIONS)
+    
+    # fill compounds table
+    for compound_id, count in counts:
+        compounds_t.AddRowData(compound_id, count)
+    
+    # fill formulas tables
+    formula_id = 0
+    for formula in formulas:
+        
+        # increase formula ID
+        formula_id += 1
+        
+        # get values
+        compound_ids, composition_ids, mw, atoms, scores = formulas[formula]
+        C = atoms.get('C', 0)
+        H = atoms.get('H', 0)
+        N = atoms.get('N', 0)
+        O = atoms.get('O', 0)
+        S = atoms.get('S', 0)
+        P = atoms.get('P', 0)
+        F = atoms.get('F', 0)
+        
+        # add to main table
+        formulas_t.AddRowData(formula_id, formula, mw, len(compound_ids), C, H, N, O, S, P, F)
+        
+        # add to connection tables
+        for compound_id in compound_ids:
+            formulas_compounds_t.AddRowData(formula_id, compound_id, scores[compound_id][0], scores[compound_id][1])
+        
+        for composition_id in composition_ids:
+            formulas_compositions_t.AddRowData(formula_id, composition_id[0], composition_id[1])
 
-# define formulas table
-path = os.path.join(response.WorkingDir, "formulas.txt")
-formulas_t = response.AddTable(TB_FORMULAS, path)
-formulas_t.AddColumn("ID", pyeds.scripting.INT, pyeds.scripting.ID)
-formulas_t.AddColumn("Formula", pyeds.scripting.STRING)
-formulas_t.AddColumn("Molecular Weight", pyeds.scripting.FLOAT)
-formulas_t.AddColumn("# C", pyeds.scripting.INT)
-formulas_t.AddColumn("# H", pyeds.scripting.INT)
-formulas_t.AddColumn("# N", pyeds.scripting.INT)
-formulas_t.AddColumn("# O", pyeds.scripting.INT)
-formulas_t.AddColumn("# S", pyeds.scripting.INT)
-formulas_t.AddColumn("# P", pyeds.scripting.INT)
-formulas_t.AddColumn("# F", pyeds.scripting.INT)
 
-# define formula to compounds link table
-# make sure all ID columns starts with the table name followed by the ID column name
-# note that currently only the strings are supported for custom connection properties
-path = os.path.join(response.WorkingDir, "formulas_compounds.txt")
-formulas_compounds_t = response.AddConnection("Formulas_Compounds", path, TB_FORMULAS, TB_COMPOUNDS)
-formulas_compounds_t.AddColumn(f"{TB_FORMULAS} ID", pyeds.scripting.INT, pyeds.scripting.ID)
-formulas_compounds_t.AddColumn(f"{TB_COMPOUNDS} ID", pyeds.scripting.INT, pyeds.scripting.ID)
-formulas_compounds_t.AddColumn("Formula Rank", pyeds.scripting.STRING)
-formulas_compounds_t.AddColumn("Formula SFit", pyeds.scripting.STRING)
-
-# define formula to compositions link table
-# make sure all ID columns starts with the table name followed by the ID column name
-path = os.path.join(response.WorkingDir, "formulas_compositions.txt")
-formulas_compositions_t = response.AddConnection("Formulas_Compositions", path, TB_FORMULAS, TB_COMPOSITIONS)
-formulas_compositions_t.AddColumn(f"{TB_FORMULAS} ID", pyeds.scripting.INT, pyeds.scripting.ID)
-formulas_compositions_t.AddColumn(f"{TB_COMPOSITIONS} ID", pyeds.scripting.INT, pyeds.scripting.ID)
-formulas_compositions_t.AddColumn(f"{TB_COMPOSITIONS} WorkflowID", pyeds.scripting.INT, pyeds.scripting.WORKFLOW_ID)
-
-# show message
-print("Exporting data...")
-
-# export response definition
-response.Save()
-
-# export tables
-with open(formulas_t.DataFile, 'w', encoding='utf-8') as wf:
-    wf.write(formulas_t.Header+"\n")
-    wf.write("\n".join(formulas_table))
-
-with open(formulas_compounds_t.DataFile, 'w', encoding='utf-8') as wf:
-    wf.write(formulas_compounds_t.Header+"\n")
-    wf.write("\n".join(formulas_compounds_table))
-
-with open(formulas_compositions_t.DataFile, 'w', encoding='utf-8') as wf:
-    wf.write(formulas_compositions_t.Header+"\n")
-    wf.write("\n".join(formulas_compositions_table))
+if __name__ == "__main__":
+    
+    # read node args
+    node_args = pyeds.scripting.NodeArgs(sys.argv)
+    
+    # collect and process all compounds and predicted compositions
+    unique_formulas, formulas_counts = collect_formulas(node_args.ResultFilePath)
+    
+    # initialize node response with all tables to be exported
+    node_response = init_response(node_args.ExpectedResponsePath)
+    
+    # populate tables with data
+    fill_tables(node_response, unique_formulas, formulas_counts)
+    
+    # export response definition and all tables data
+    node_response.Export()
